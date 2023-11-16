6992d5b3ee0930e6d6c647650d474446131a1af7
Louis Boeckmans <louis.boeckmans@outlook.com>
2023-11-16 07:22:49 +0100

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { SidebarComponent } from './navigation/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, 
+            RouterOutlet,
+            RouterLink,
+            RouterLinkActive,
+            SidebarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LayoutsComponent } from './components/layouts/layouts.component';
+import { HorizontalLayoutsComponent } from './components/layouts/horizontal-layouts/horizontal-layouts.component';
+import { VerticalLayoutsComponent } from './components/layouts/vertical-layouts/vertical-layouts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'layouts' , component: LayoutsComponent},
+    { path: 'layouts/horizontal-layouts' , component: HorizontalLayoutsComponent},
+    { path: 'layouts/vertical-layouts' , component: VerticalLayoutsComponent},
+];
